51e4a064c7a8a8a970cc85ee27cd05f88f1f1c78
savelyi <leshaboichenko95@mail.ru>
2024-10-20 02:49:28 +0300

    add html and css files

---
+++ index.js
@@ -0,0 +1,16 @@
+// const table = document.querySelector('.table');
+// const tableLegs = document.querySelector('.table-legs');
+// const raiseButton = document.getElementById('raise-table');
+// const lowerButton = document.getElementById('lower-table');
+
+// let tablePosition = 0; // Начальная позиция стола
+
+// raiseButton.addEventListener('click', () => {
+//   tablePosition -= 10; // Поднимаем стол на 10px (или другое значение)
+//   table.style.transform = `translateY(${tablePosition}px)`; // Применяем смещение
+// });
+
+// lowerButton.addEventListener('click', () => {
+//   tablePosition += 10; // Опускаем стол на 10px
+//   table.style.transform = `translateY(${tablePosition}px)`; // Применяем смещение
+// });
